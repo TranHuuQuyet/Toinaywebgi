ce67403ec248952e4600bffda5dae57b7618a181
Orange_Dev <huuquyet120406@gmail.com>
2026-09-16 22:33:06 +0700

    feat: connect deployed Worker API (worker/src/index.js)

--- worker/src/index.js
+++ worker/src/index.js
@@ -9,6 +9,8 @@
 
 const ALLOWED_ORIGINS = [
   'https://tranhuuquyet.github.io',
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
   'http://localhost:4173',
   'http://localhost:3000',
   'http://127.0.0.1:4173',
